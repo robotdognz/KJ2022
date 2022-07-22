c0a7aae4b2d8fb40b25cfd9e5e1b436074fc33c5
robotdognz <robotdognz@gmail.com>
2022-07-22 20:46:35 +1200

    Jank first pass title screen and pause menu

---
+++ Assets/Scripts/UI/MenuLogic.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuLogic : MonoBehaviour
+{
+    public void StartGame()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(1);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void TitleScreen()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(0);
+    }
+
+    public void PauseGame()
+    {
+        Time.timeScale = 0;
+    }
+
+    public void ResumeGame()
+    {
+        Time.timeScale = 1;
+    }
+}
