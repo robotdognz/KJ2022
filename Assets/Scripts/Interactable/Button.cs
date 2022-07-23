0879377fb0b3e0fd94eb05bbd2ad64d398aef412
robotdognz <robotdognz@gmail.com>
2022-07-23 15:37:46 +1200

    Button is working great now

--- Assets/Scripts/Interactable/Button.cs
+++ Assets/Scripts/Interactable/Button.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,6 +6,8 @@ using UnityEngine;
 public class Button : Activate
 {
     Trigger trigger;
+    private bool isEntering;
+    private bool isExiting;
 
     void Start()
     {
@@ -15,12 +18,28 @@ public class Button : Activate
 
     void Enter()
     {
+        if (!isEntering) StartCoroutine(EnterEnum());
+    }
+
+    private IEnumerator EnterEnum()
+    {
+        isEntering = true;
         DoActivate();
+        yield return new WaitForSeconds(0.2f);
+        isEntering = false;
     }
 
     void Exit()
     {
+        if (!isExiting) StartCoroutine(ExitEnum());
+    }
+    
+    private IEnumerator ExitEnum()
+    {
+        isExiting = true;
         DoDeactivate();
+        yield return new WaitForSeconds(0.2f);
+        isExiting = false;
     }
 
 }
